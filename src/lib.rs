6721e84728e1f19fe26d46509d8aca67e6f3f2a9
cwbhhjl <cwbhhjl@gmail.com>
2023-10-16 09:33:55 +0800

    merkle with escape

--- src/lib.rs
+++ src/lib.rs
@@ -194,6 +194,94 @@ impl Merkle {
             self.root[3] = merkle_getroot();
         }
     }
+
+    pub fn get_with_escape(&self, index: u64, data: &mut [u64], pad: bool, escape: usize) -> u64 {
+        let mut hash = [0; 4];
+        unsafe {
+            merkle_address(index);
+
+            merkle_setroot(self.root[0]);
+            merkle_setroot(self.root[1]);
+            merkle_setroot(self.root[2]);
+            merkle_setroot(self.root[3]);
+
+            hash[0] = merkle_get();
+            hash[1] = merkle_get();
+            hash[2] = merkle_get();
+            hash[3] = merkle_get();
+
+            //enforce root does not change
+            merkle_getroot();
+            merkle_getroot();
+            merkle_getroot();
+            merkle_getroot();
+
+            let len = merkle_fetch_data();
+            if len>0 {
+                require(len <= data.len() as u64 && len as usize >= escape);
+                for i in 0..len {
+                    data[i as usize] = merkle_fetch_data();
+                }
+
+                // FIXME: avoid copy here
+                let hash_check = PoseidonHasher::hash(&data[0..len as usize - escape], pad);
+                require(hash[0] == hash_check[0]);
+                require(hash[1] == hash_check[1]);
+                require(hash[2] == hash_check[2]);
+                require(hash[3] == hash_check[3]);
+            }
+            len
+        }
+    }
+
+
+    pub fn set_with_escape(&mut self, index: u64, data: &[u64], pad: bool, escape: usize) {
+        // place a dummy get for merkle proof convension
+        unsafe {
+            merkle_address(index);
+
+            merkle_setroot(self.root[0]);
+            merkle_setroot(self.root[1]);
+            merkle_setroot(self.root[2]);
+            merkle_setroot(self.root[3]);
+
+            merkle_get();
+            merkle_get();
+            merkle_get();
+            merkle_get();
+
+            //enforce root does not change
+            merkle_getroot();
+            merkle_getroot();
+            merkle_getroot();
+            merkle_getroot();
+        }
+
+        unsafe {
+            merkle_address(index);
+
+            merkle_setroot(self.root[0]);
+            merkle_setroot(self.root[1]);
+            merkle_setroot(self.root[2]);
+            merkle_setroot(self.root[3]);
+
+            for d in data.iter() {
+                merkle_put_data(*d);
+            }
+            require(data.len() > escape);
+
+            let hash = PoseidonHasher::hash(&data[..data.len() - escape], pad);
+            merkle_set(hash[0]);
+            merkle_set(hash[1]);
+            merkle_set(hash[2]);
+            merkle_set(hash[3]);
+
+            self.root[0] = merkle_getroot();
+            self.root[1] = merkle_getroot();
+            self.root[2] = merkle_getroot();
+            self.root[3] = merkle_getroot();
+        }
+    }
 }
 
 pub struct PoseidonHasher (u64);
